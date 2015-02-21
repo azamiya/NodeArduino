c2697e34aa62d61f9def22c2373390242cbac99a
Tatsuki Adaniya <t.adaniya0624@gmail.com>
2015-02-20 23:56:01 -0800

    change servo.js

--- NodeArduino_Servotest/servo_test.js
+++ NodeArduino_Servotest/servo_test.js
@@ -1,13 +1,10 @@
-var five = require("johnny-five");
-
 var app = require('http').createServer(handler), 
     io = require('/usr/local/lib/node_modules/socket.io').listen(app), 
     fs = require('fs'),
     firmata = require('/usr/local/lib/node_modules/firmata'),
-    //board = new firmata.Board('/dev/ttyACM0', arduinoReady);
-    board = new five.Board('/dev/ttyACM0', arduinoReady);
+    board = new firmata.Board('/dev/ttyACM0', arduinoReady);
  
-var ledPin = 9;
+var ledPin = 13;
  
 function arduinoReady(err) {
     if (err) {
@@ -20,12 +17,8 @@ function arduinoReady(err) {
  
     var ledOn = true;
     board.pinMode(ledPin, board.MODES.OUTPUT);
-    console.log("line23");
-
 }
  
-
-
 app.listen(8080);
 console.log("Listening on http://raspberrypi:8080...");
  
@@ -46,49 +39,24 @@ function handler (req, res) {
  
 // this handles socket.io comm from html files
  
-board.on("ready", function(){ 
-    console.log("line50");
-    var servo = new five.Servo(9);
-    io.sockets.on('connection', function(socket) {
-        socket.send('connected...');
-
-        socket.on('emit_from_client', function(data){
-            console.log(data);
-            //socket.emit('emit_from_server', 'hello from server: ' + data);
-            if(data > 0 && data < 10){
-                servo.to(data);
-                data = dig;
-            }
-        });
-
-        socket.on('message', function(data) {
-            if (data == 'turn on') {
-                console.log('+');
-                board.digitalWrite(ledPin, 30);
-                servo.to(30);
-
-                socket.broadcast.send("let there be light!");
-            }
-            if (data == 'turn off') {
-                console.log('-');
-                board.digitalWrite(ledPin, 10);
-                servo.to(10);
-                socket.broadcast.send("who turned out the light?");
-            }
-            /*if(data == 'left'){
-                dig = dig - 5;
-                servo.to(dig);
-            }
-            if(data == 'right'){
-                dig = dig + 5;
-                servo.to(dig);
-            }*/
-            return;
-        });
-     
-        socket.on('disconnect', function() {
-            socket.send('disconnected...');
-        });
+io.sockets.on('connection', function(socket) {
+    socket.send('connected...');
+    
+    socket.on('message', function(data) {
+        if (data == 'turn on') {
+            console.log('+');
+            board.digitalWrite(ledPin, board.HIGH);
+            socket.broadcast.send("let there be light!");
+        }
+        if (data == 'turn off') {
+            console.log('-');
+            board.digitalWrite(ledPin, board.LOW);
+            socket.broadcast.send("who turned out the light?");
+        }
+        return;
+    });
+ 
+    socket.on('disconnect', function() {
+        socket.send('disconnected...');
     });
 });
-

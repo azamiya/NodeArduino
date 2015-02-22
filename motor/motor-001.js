b454bcf08a5db246eccb136d372f33fcc3d9f204
Tatsuki Adaniya <t.adaniya0624@gmail.com>
2015-02-22 02:04:55 -0800

    change spd

--- motor/motor-001.js
+++ motor/motor-001.js
@@ -3,22 +3,22 @@ var board = new five.Board();
 
 board.on("ready", function() {
   // Assuming an Led is attached to pin 13, this will turn it on
-  this.pinMode(3, five.Pin.ANALOG);
+  /*this.pinMode(3, five.Pin.ANALOG);
   this.analogWrite(3, 10);
 
   this.pinMode(4, five.Pin.OUTPUT);
   this.digitalWrite(4, 1);
 
   this.pinMode(7, five.Pin.OUTPUT);
-  this.digitalWrite(7, 0);
+  this.digitalWrite(7, 0);*/
 
-  /*this.pinMode(6, five.Pin.ANALOG);
+  this.pinMode(6, five.Pin.ANALOG);
   this.analogWrite(6, 10);
 
   this.pinMode(8, five.Pin.OUTPUT);
   this.digitalWrite(8, 1);
 
   this.pinMode(12, five.Pin.OUTPUT);
-  this.digitalWrite(12, 0);*/
+  this.digitalWrite(12, 0);
 
 });

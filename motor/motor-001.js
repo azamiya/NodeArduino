9c0ae932e1e701e5510864cc15287a6a1f4642cb
Tatsuki Adaniya <t.adaniya0624@gmail.com>
2015-02-22 01:58:24 -0800

    change spd

--- motor/motor-001.js
+++ motor/motor-001.js
@@ -6,19 +6,19 @@ board.on("ready", function() {
   this.pinMode(3, five.Pin.ANALOG);
   this.analogWrite(3, 10);
 
-  this.pinMode(6, five.Pin.OUTPUT);
-  this.digitalWrite(6, 0);
+  this.pinMode(4, five.Pin.OUTPUT);
+  this.digitalWrite(6, 1);
 
   this.pinMode(7, five.Pin.OUTPUT);
   this.digitalWrite(7, 0);
 
-  this.pinMode(5, five.Pin.ANALOG);
+  this.pinMode(6, five.Pin.ANALOG);
   this.analogWrite(5, 10);
 
   this.pinMode(8, five.Pin.OUTPUT);
   this.digitalWrite(8, 1);
 
-  this.pinMode(9, five.Pin.OUTPUT);
-  this.digitalWrite(9, 0);
+  this.pinMode(12, five.Pin.OUTPUT);
+  this.digitalWrite(12, 0);
 
 });

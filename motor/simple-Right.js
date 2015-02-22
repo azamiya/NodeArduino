0c9949df1485edb650f042bee79b443b96df8d4d
Tatsuki Adaniya <t.adaniya0624@gmail.com>
2015-02-21 23:33:55 -0800

    change spd

---
+++ motor/simple-Right.js
@@ -0,0 +1,31 @@
+var five = require("johnny-five"), 
+    board = new five.Board();
+
+board.on("ready", function() {
+
+   var motor_R = new five.Motor({
+    pins: {
+      pwm: 10,
+      dir: 9,
+      brake: 8
+    }
+  });
+
+  motor_R.on("forward", function(err, timestamp) {
+    // demonstrate braking after 5 seconds
+    board.wait(50, function() {
+      motor_R.brake();
+    });
+  });
+
+  motor_R.on("brake", function(err, timestamp) {
+    // Release the brake after .1 seconds
+    board.wait(10, function() {
+      motor_R.stop();
+    });
+  });
+
+  motor_R.forward(10);
+
+
+});
